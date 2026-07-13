51d6f78cd0a23d3a0660a91083bef57dd710e1ba
Derrick Williams <derrick.williams@rainforestpay.com>
2026-07-13 08:24:12 -0400

    Update to 0.5.1

--- Package.swift
+++ Package.swift
@@ -19,13 +19,13 @@ let package = Package(
         ),
         .binaryTarget(
             name: "RainforestSDKFramework",
-            url: "https://static.rainforestpay.com/tap-to-phone/rainforest-sdk.0.4.1.zip",
-            checksum: "490b88991ca813aeec48b9154237969599d2af215f1086884a2e058c4fe13c92"
+            url: "https://static.rainforestpay.com/tap-to-phone/rainforest-sdk.0.5.1.zip",
+            checksum: "1b16d5da83e6620ea7773cc1c5bdf477f508fe54bdbb6a411b1c681963a43db0"
         ),
         .binaryTarget(
             name: "CloudCommerceFramework",
-            url: "https://static.rainforestpay.com/tap-to-phone/cloud-commerce.0.4.1.zip",
-            checksum: "1a26e17cba8e07e3ce9417269e9b1770f217dd7d5d568be33b2985b51b03cd72"
+            url: "https://static.rainforestpay.com/tap-to-phone/cloud-commerce.0.5.1.zip",
+            checksum: "4aac8455f7ef5f31f03ee71f09a69d2f71620d65efc125c67261ad75f21b9a34"
         ),
     ]
 )
